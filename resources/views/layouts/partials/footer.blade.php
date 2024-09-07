ced84e229665a3ff39fb684917777f7e4445486b
drjinwoo <dansalivio2001@gmail.com>
2024-09-08 02:29:53 +0800

    First Commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>{{ __('common.Version') }}</b> 1.0.1
+    </div>
+    <strong>{{ __('common.Copyright') }} &copy; 2024 <a>G & G Store</a>.</strong> {{ __('common.all_reserved') }}
+  </footer>
